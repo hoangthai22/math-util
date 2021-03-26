792913c7e4119ead35df4ee2f37fd557f54f0cea
hoangthai22 <hoangthai@gmail.com>
2021-03-26 07:47:59 +0700

    Update core: using recursion. And GREEN 4EVER

--- src/com/thai/mathutil/MathUtility.java
+++ src/com/thai/mathutil/MathUtility.java
@@ -20,6 +20,20 @@ public class MathUtility {
     //kiểu long chịu dc 10^18 18 con số 0.Lớn hơn nữa là thua
     //hàm chỉ chấp nhận từ 0--20! => long
     //0! = 1! =1
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument . n must be between 0..20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        //n! = 1.2.3.4.5...n
+//        return result;
+//    }
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument . n must be between 0..20");
@@ -27,11 +41,6 @@ public class MathUtility {
         if (n == 0 || n == 1) {
             return 1;
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        //n! = 1.2.3.4.5...n
-        return result;
+        return n * getFactorial(n - 1);
     }
 }
